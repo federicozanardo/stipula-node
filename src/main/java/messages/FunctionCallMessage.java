a7a186e878b6b171f842357a271749c04bd98b28
Federico Zanardo <federicozanardo98@gmail.com>
2022-12-29 00:25:28 +0100

    feat: Create the FunctionCallMessage object

--- src/main/java/messages/FunctionCallMessage.java
+++ src/main/java/messages/FunctionCallMessage.java
@@ -1,4 +1,33 @@
 package messages;
 
+import java.util.HashMap;
+
 public class FunctionCallMessage extends Message {
+    private final String contractId;
+    private final String contractInstanceId;
+    private final String function;
+    private final HashMap<String, String> arguments;
+
+    public FunctionCallMessage(String contractId, String contractInstanceId, String function, HashMap<String, String> arguments) {
+        this.contractId = contractId;
+        this.contractInstanceId = contractInstanceId;
+        this.function = function;
+        this.arguments = arguments;
+    }
+
+    public String getContractId() {
+        return contractId;
+    }
+
+    public String getContractInstanceId() {
+        return contractInstanceId;
+    }
+
+    public String getFunction() {
+        return function;
+    }
+
+    public HashMap<String, String> getArguments() {
+        return arguments;
+    }
 }
