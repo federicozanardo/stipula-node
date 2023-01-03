e09d93b2e9f96fc77971ce82bd28e03c52b25e78
Federico Zanardo <federicozanardo98@gmail.com>
2023-01-03 11:04:25 +0100

    feat: Refactor Single-Use Seal

--- src/main/java/vm/contract/SingleUseSeal.java
+++ src/main/java/vm/contract/SingleUseSeal.java
@@ -1,6 +1,34 @@
 package vm.contract;
 
-public class SingleUseSeal<T> {
-    private T property;
-    private String lock;
+import asset.AssetConfig;
+import vm.types.FloatType;
+
+public class SingleUseSeal<T extends AssetConfig> {
+    private final String id;
+    private final T assetConfig;
+    private final FloatType amount;
+    private final String lockingScript;
+
+    public SingleUseSeal(String id, T assetConfig, FloatType amount, String publicKeyHash) {
+        this.id = id;
+        this.assetConfig = assetConfig;
+        this.amount = amount;
+        this.lockingScript = "DUP\n SHA256\n PUSH str " + publicKeyHash + "\n EQUAL\n CHECKSIG\n";
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public T getAssetConfig() {
+        return assetConfig;
+    }
+
+    public FloatType getAmount() {
+        return amount;
+    }
+
+    public String getLockingScript() {
+        return lockingScript;
+    }
 }
