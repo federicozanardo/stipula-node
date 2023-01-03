8d1c62f3c60c203207dfe531311a00bea3153092
Federico Zanardo <federicozanardo98@gmail.com>
2023-01-03 14:28:37 +0100

    feat: Add asset arguments
    This is a particular field in order to parse and to check the asset past as arguments via function call

--- src/main/java/messages/FunctionCallMessage.java
+++ src/main/java/messages/FunctionCallMessage.java
@@ -7,12 +7,22 @@ public class FunctionCallMessage extends Message {
     private final String contractInstanceId;
     private final String function;
     private final HashMap<String, String> arguments;
+    private final HashMap<String, PayToContract> assetArguments;
 
     public FunctionCallMessage(String contractId, String contractInstanceId, String function, HashMap<String, String> arguments) {
         this.contractId = contractId;
         this.contractInstanceId = contractInstanceId;
         this.function = function;
         this.arguments = arguments;
+        this.assetArguments = null;
+    }
+
+    public FunctionCallMessage(String contractId, String contractInstanceId, String function, HashMap<String, String> arguments, HashMap<String, PayToContract> assetArguments) {
+        this.contractId = contractId;
+        this.contractInstanceId = contractInstanceId;
+        this.function = function;
+        this.arguments = arguments;
+        this.assetArguments = assetArguments;
     }
 
     public String getContractId() {
@@ -29,5 +39,9 @@ public class FunctionCallMessage extends Message {
 
     public HashMap<String, String> getArguments() {
         return arguments;
+    }
+
+    public HashMap<String, PayToContract> getAssetArguments() {
+        return assetArguments;
     }
 }
