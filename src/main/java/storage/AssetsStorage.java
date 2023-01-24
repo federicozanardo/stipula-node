990808805d8f7120ce5e9dd0d4f1b88b6900ea99
Federico Zanardo <federicozanardo98@gmail.com>
2023-01-25 00:19:33 +0100

    feat: Implement assets storage

---
+++ src/main/java/storage/AssetsStorage.java
@@ -0,0 +1,62 @@
+package storage;
+
+import constants.Constants;
+import models.assets.Asset;
+import models.assets.AssetConfig;
+import models.assets.FungibleAsset;
+import org.iq80.leveldb.DB;
+import org.iq80.leveldb.Options;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.UUID;
+import java.util.concurrent.locks.ReentrantLock;
+
+import static org.iq80.leveldb.impl.Iq80DBFactory.bytes;
+import static org.iq80.leveldb.impl.Iq80DBFactory.factory;
+
+public class AssetsStorage extends StorageSerializer<Asset> {
+    private DB levelDb;
+    private final ReentrantLock mutex;
+
+    public AssetsStorage() {
+        this.mutex = new ReentrantLock();
+    }
+
+    public void seed() throws IOException {
+        FungibleAsset bitcoin = new FungibleAsset("Bitcoin", "BTC", 10000, 2);
+        String assetId = this.addAsset(bitcoin);
+        System.out.println("seed: assetId => " + assetId);
+    }
+
+    public String addAsset(AssetConfig assetConfig) throws IOException {
+        this.mutex.lock();
+
+        // TODO: check that the id is unique
+        String assetId = UUID.randomUUID().toString();
+        Asset asset = new Asset(assetId, assetConfig);
+
+        this.levelDb = factory.open(new File(String.valueOf(Constants.ASSETS_PATH)), new Options());
+        this.levelDb.put(bytes(assetId), this.serialize(asset));
+        this.levelDb.close();
+
+        this.mutex.unlock();
+        return assetId;
+    }
+
+    public Asset getAsset(String assetId) throws IOException {
+        this.mutex.lock();
+        this.levelDb = factory.open(new File(String.valueOf(Constants.ASSETS_PATH)), new Options());
+
+        Asset asset = this.deserialize(this.levelDb.get(bytes(assetId)));
+        if (asset == null) {
+            this.mutex.unlock();
+            // Error: this contractInstanceId does not exist
+            return null;
+        }
+
+        this.levelDb.close();
+        this.mutex.unlock();
+        return asset;
+    }
+}
