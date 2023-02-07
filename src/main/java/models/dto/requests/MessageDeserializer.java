3e9402baf7bb019ea9f59acb68eee8d2705e9b96
Federico Zanardo <federicozanardo98@gmail.com>
2023-02-07 11:50:33 +0100

    fix: Update MessageDeserializer

--- src/main/java/models/dto/requests/MessageDeserializer.java
+++ src/main/java/models/dto/requests/MessageDeserializer.java
@@ -1,8 +1,14 @@
 package models.dto.requests;
 
 import com.google.gson.*;
+import exceptions.models.dto.requests.contract.function.UnsupportedTypeException;
+import models.contract.PayToContract;
+import models.dto.requests.contract.FunctionArgument;
+import models.dto.requests.contract.agreement.AgreementCall;
+import models.dto.requests.contract.function.FunctionCall;
 
 import java.lang.reflect.Type;
+import java.util.ArrayList;
 import java.util.HashMap;
 
 public class MessageDeserializer implements JsonDeserializer<Message> {
@@ -18,13 +24,50 @@ public class MessageDeserializer implements JsonDeserializer<Message> {
 
     @Override
     public Message deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context) throws JsonParseException {
+        ArrayList<FunctionArgument> args = new ArrayList<>();
+
         JsonObject jsonObject = json.getAsJsonObject();
-        String value = jsonObject.get("className").getAsString();
+        JsonArray arguments = jsonObject.get("arguments").getAsJsonArray();
+
+        for (int i = 0; i < arguments.size(); i++) {
+            JsonObject argument = arguments.get(i).getAsJsonObject().get("argument").getAsJsonObject();
+            String argumentType = argument.get("first").getAsString();
+            String variableName = argument.get("second").getAsString();
+
+            if (argumentType.equals("asset")) {
+                JsonObject val = argument.get("third").getAsJsonObject();
+                String propertyId = val.get("propertyId").getAsString();
+                String address = val.get("address").getAsString();
+                String unlockScript = val.get("unlockScript").getAsString();
+
+                try {
+                    args.add(new FunctionArgument(argumentType, variableName, new PayToContract(propertyId, address, unlockScript)));
+                } catch (UnsupportedTypeException e) {
+                    throw new RuntimeException(e);
+                }
+            } else {
+                String val = argument.get("third").getAsString();
+                try {
+                    args.add(new FunctionArgument(argumentType, variableName, val));
+                } catch (UnsupportedTypeException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }
+        String value = jsonObject.get("type").getAsString();
 
         for (HashMap.Entry<String, Class<? extends Message>> entry : dataTypeRegistry.entrySet()) {
             if (value.equals(entry.getKey())) {
                 Class<? extends Message> dataType = dataTypeRegistry.get(value);
-                return context.deserialize(jsonObject, dataType);
+                Message message = context.deserialize(jsonObject, dataType);
+
+                if (message instanceof FunctionCall) {
+                    FunctionCall functionCall = (FunctionCall) message;
+                    return new FunctionCall(functionCall.getContractInstanceId(), functionCall.getFunctionName(), args);
+                } else {
+                    AgreementCall agreementCall = (AgreementCall) message;
+                    return new AgreementCall(agreementCall.getContractId(), args, agreementCall.getParties());
+                }
             }
         }
 
