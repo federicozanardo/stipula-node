f4c26e22dca1f692bf525afb26260552dc6fd542
Federico Zanardo <federicozanardo98@gmail.com>
2023-02-09 01:03:15 +0100

    feat: Create a test example

---
+++ src/test/java/MyFirstTest.java
@@ -0,0 +1,8 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+public class MyFirstTest {
+    @Test
+    public void firstTest() {
+        Assertions.assertTrue(true);
+    }
+}
